60983138d2987a16d731ad74d372f5e84f0cb3e7
Md Roknujjaman Rony <roknujjamanrony1234@gmail.com>
2025-07-02 10:15:32 +0600

    explore crud with postman

--- index.js
+++ index.js
@@ -58,6 +58,44 @@ app.post("/blog", async (req, res) => {
   }
 });
 
+app.get("/blog", async (req, res) => {
+  try {
+    const blogs = await Blog.find();
+    res.status(200).json(blogs);
+  } catch (error) {
+    console.error("Error fetching blogs:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+//for getting a single blog by id
+app.get("/blog/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const blogs = await Blog.find({ _id: id });
+    res.status(200).json(blogs);
+  } catch (error) {
+    console.error("Error fetching blogs:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+//delete a blog by id
+app.delete("/blog/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const deletedBlog = await Blog.findOne({ _id: id });
+    if (!deletedBlog) {
+      return res.status(404).json({ message: "Blog not found" });
+    }
+    await Blog.deleteOne({ _id: id });
+    res.status(200).json({ message: "Blog deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting blog:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
 // Server Listener
 app.listen(port, async () => {
   console.log(`🚀 Server is running at http://localhost:${port}`);
